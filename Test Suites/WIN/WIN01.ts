2b2197f1d15beb6d175dd6d06b155a62f2666405
Phuong Oanh Nguyen Thi <nhokk0509@gmail.com>
2024-06-12 14:44:31 +0700

    add WIN TC

---
+++ Test Suites/WIN/WIN01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WIN01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>396cc77c-b5fb-4b10-a4f4-959a06d92c41</testSuiteGuid>
+   <testCaseLink>
+      <guid>c46167a7-3f11-410d-8b48-afd3056f24db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WIN/TC 01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
